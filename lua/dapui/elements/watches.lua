04a829b840a53b7303902799f34a83eb762415f4
Markus Friedrich <fm12111+github@googlemail.com>
2022-12-26 22:38:29 +0100

    Allow to add a new watch expression via scripting

--- lua/dapui/elements/watches.lua
+++ lua/dapui/elements/watches.lua
@@ -14,4 +14,17 @@ return {
   render = function(canvas)
     watches:render(canvas)
   end,
+  add_expression = function(expr)
+    if not expr then
+      if vim.fn.mode() == "v" then
+        local start = vim.fn.getpos("v")
+        local finish = vim.fn.getpos(".")
+        local lines = require("dapui.util").get_selection(start, finish)
+        expr = table.concat(lines, "\n")
+      else
+        expr = expr or vim.fn.expand("<cexpr>")
+      end
+    end
+    watches:add_watch(expr)
+  end,
 }
